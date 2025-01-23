3d2205870e926f1ed3983df2792fdcbb93cdad92
O. Arowolaju <31176887+oluaju@users.noreply.github.com>
2025-01-22 22:43:36 -0600

    fix hard coded redirects

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -5,7 +5,7 @@ import { google } from 'googleapis';
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  'http://localhost:3000/api/auth/google/callback'
+  process.env.GOOGLE_REDIRECT_URI || `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google/callback`
 );
 
 export async function POST() {
